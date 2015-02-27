5207e03c722edef5dc1033c72db01528445fbccd
DineshB <Dinesh.Balachandran@hotmail.com>
2015-02-27 09:29:42 -0600

    Adding Class, and ref files

---
+++ GARUD.Entity/ColumnDesignCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Policy;
+
+namespace GARUD.Entity
+{
+    public class ColumnDesignCheck
+    {
+        public string TableName { get; set; }
+
+        public string SchemaName { get; set; }
+
+        public string ColumnName { get; set; }
+
+        public string PrimaryColumnName { get; set; }
+
+        public string NullableFieldMismatch { get; set; }
+
+        public string MaxSizeMismatch { get; set; }
+
+        public string OctetSizeMismatch { get; set; }
+
+        public string DataTypeMismatch { get; set; }
+    }
+}
---
+++ GARUD.Entity/DatabaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GARUD.Entity
+{
+    public class DatabaseEntity 
+    {
+        public List<string> DatabaseNames { get; set; }
+
+
+        public ObservableCollection<TestCase> TestCaseList { get; set; }
+    }
+}
